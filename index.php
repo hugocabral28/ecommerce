bed69015e3dc2fae5a676de95c603fcfdd618812
Hugo Cabral <hugocabral2803@gmail.com>
2018-07-28 23:10:46 -0300

    tela de login

--- index.php
+++ index.php
@@ -1,9 +1,11 @@
-<?php 
-
+<?php
+session_start();
 require_once("vendor/autoload.php");
+
 use \Slim\Slim;
 use \Hcode\Page;
 use \Hcode\PageAdmin;
+use \Hcode\Model\User;
 
 $app = new Slim();
 
@@ -18,10 +20,32 @@ $app->get('/', function() {
 $app->config('debug', true);
 
 $app->get('/admin', function() {
+	User::verifyLogin();
 
 	$Page = new PageAdmin();
 	$Page->setTpl("index");
 });
+
+$app->get('/admin/login', function() {
+	$Page = new PageAdmin([
+		"header"=>false,
+		"footer"=>false
+	]);
+	$Page->setTpl("login");
+});
+
+$app->post('/admin/login', function(){
+	User::login($_POST["login"],$_POST["password"]);
+	header("Location: /admin");
+	exit;
+});
+
+$app->get('/admin/logout', function(){
+	User::logout();
+	header("Location: /admin/login");
+	exit;
+});
+
 $app->run();
 
  ?>
--- vendor/hcodebr/php-classes/src/Page.php
+++ vendor/hcodebr/php-classes/src/Page.php
@@ -2,13 +2,12 @@
 namespace Hcode;
 use Rain\Tpl;
 
-/**
- * 
- */
 class Page {
 	private $tpl;
 	private $options =[];
 	private $defaults = [
+		"header"=>true,
+		"footer"=>true,
 		"data"=>[]
 	];
 	public function __construct($opts = array(),$tpl_dir = "/views/")
@@ -26,7 +25,7 @@ class Page {
 		$this->tpl = new Tpl;
 		$this->setData($this->options["data"]);
 
-		$this->tpl->draw("header");
+		if ($this->options["header"] === true) $this->tpl->draw("header");
 	}
 	private function setData($data = array()){
 		foreach ($data as $key => $value) {
@@ -40,7 +39,7 @@ class Page {
 	}
 	public function  __destruct()
 	{
-		$this->tpl->draw("footer");
+		if ($this->options["footer"] === true) $this->tpl->draw("footer");
 	}
 
 }
