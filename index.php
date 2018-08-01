80d8b124008147f3b1e28ca7af05332beab2c239
Hugo Cabral <hugocabral2803@gmail.com>
2018-07-31 21:48:22 -0300

    Admin - Categorias

--- index.php
+++ index.php
@@ -6,6 +6,7 @@ use \Slim\Slim;
 use \Hcode\Page;
 use \Hcode\PageAdmin;
 use \Hcode\Model\User;
+use \Hcode\Model\Category;
 
 $app = new Slim();
 
@@ -131,6 +132,57 @@ $app->get("/admin/forgot/sent", function(){
 		"footer"=>false
 	]);
 	$Page->setTpl("forgot-sent");
+});
+
+$app->get("/admin/categories", function(){
+	User::verifyLogin();
+	$categories = Category::listAll();
+	$Page = new PageAdmin();
+		$Page->setTpl("categories",[
+			'categories'=>$categories
+		]);
+});
+
+$app->get("/admin/categories/create", function(){
+		User::verifyLogin();
+		$Page = new PageAdmin();
+		$Page->setTpl("categories-create");
+});
+
+$app->post("/admin/categories/create", function(){
+		User::verifyLogin();
+		$category = new Category();
+		$category->setData($_POST);
+		$category->save();
+		header("Location: /admin/categories");
+		exit;
+});
+
+$app->get("/admin/categories/:idcategory/delete", function($idcategory){
+	User::verifyLogin();
+	$category = new Category();
+	$category->get((int)$idcategory);
+	$category->delete();
+	header("Location: /admin/categories");
+	exit;
+});
+$app->get("/admin/categories/:idcategory", function($idcategory){
+	User::verifyLogin();
+	$category = new Category();
+	$category->get((int)$idcategory);
+	$Page = new PageAdmin();
+	$Page->setTpl("categories-update",[
+		'category'=>$category->getValues()
+	]);
+});
+$app->post("/admin/categories/:idcategory", function($idcategory){
+	User::verifyLogin();
+	$category = new Category();
+	$category->get((int)$idcategory);
+	$category->setData($_POST);
+	$category->save();
+	header("Location: /admin/categories");
+	exit;
 });
 
 $app->run();
